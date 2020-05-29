42a1cbf49c370a710f29d3c31300b32daf0d3b50
NataliaVolik <log.litana@gmail.com>
2020-05-29 10:24:49 +0600

    add component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { PostComponent } from './post/post.component';
+import { PostsComponent } from './posts/posts.component';
+import { AboutExtraComponent } from './about-extra/about-extra.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    PostComponent,
+    PostsComponent,
+    AboutExtraComponent
   ],
   imports: [
     BrowserModule,
